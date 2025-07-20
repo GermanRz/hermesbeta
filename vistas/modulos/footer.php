389ce89861ee3d4531e3bf87e7f37d7e82fc5a1b
Jensen Ballen <jensenbanguera@gmail.com>
2025-07-20 15:10:49 -0500

    correciones minimas y footer en acerca-de

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -2,5 +2,5 @@
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 0.0.1
     </div>
-    <strong>Copyright &copy; Sena 2025 - ADSO 2847523. <a href="#">Herramientas y equipos reservables para el manejo estratégico de servicios</a>.</strong> 
+    <strong>Copyright &copy; Sena 2025 - ADSO 2847523. <a href="acerca-de">Herramientas y equipos reservables para el manejo estratégico de servicios</a>.</strong> 
 </footer>
